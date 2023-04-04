cd4218a4c3ecd6b55653a1ef0ca480d1b1e17dd2
jeremyherrera <rocafuerteherrera@gmail.com>
2023-04-04 14:57:12 -0600

    Initial commit

---
+++ app/src/main/java/com/example/mvvm/viewModel/QuoteViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.mvvm.viewModel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.mvvm.model.QuoteModel
+import com.example.mvvm.model.QuoteProvider
+
+class QuoteViewModel: ViewModel() {
+
+    val quoteModel = MutableLiveData<QuoteModel>()
+
+    fun randomQuote(){
+        val currentQuote = QuoteProvider.random()
+        quoteModel.postValue(currentQuote)
+    }
+
+}
